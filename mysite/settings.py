9afd393c8f52e7b7c28600c971bd5c172b6d8fa9
Knight9z-uzi <1329324095@qq.com>
2020-03-30 21:37:52 +0800

    post list

--- mysite/settings.py
+++ mysite/settings.py
@@ -74,25 +74,25 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'projectdb',
-#         'USER':'root',
-#         'PASSWORD':'lzh20030114',
-#         'HOST':'127.0.0.1',
-#         'PORT':'3306',
-#     }
-# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Knight9z$projectdb',
-        'USER': 'Knight9z',
-        'PASSWORD': 'Lzh20030114',
-        'HOST': 'Knight9z.mysql.pythonanywhere-services.com',
-    }
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'projectdb',
+         'USER':'root',
+         'PASSWORD':'lzh20030114',
+         'HOST':'127.0.0.1',
+         'PORT':'3306',
+     }
 }
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.mysql',
+#        'NAME': 'Knight9z$projectdb',
+#        'USER': 'Knight9z',
+#        'PASSWORD': 'Lzh20030114',
+#        'HOST': 'Knight9z.mysql.pythonanywhere-services.com',
+#    }
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
