77212bdba991699da5414b70656c19f1296d75fd
Knight9z-uzi <1329324095@qq.com>
2020-04-24 19:08:15 +0800

    some changes

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,25 +75,26 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-     'default': {
-         'ENGINE': 'django.db.backends.mysql',
-         'NAME': 'projectdb',
-         'USER':'root',
-         'PASSWORD':'lzh20030114',
-         'HOST':'127.0.0.1',
-         'PORT':'3306',
-     }
-}
+
 # DATABASES = {
 #      'default': {
 #          'ENGINE': 'django.db.backends.mysql',
-#          'NAME': 'Knight9z$projectdb',
-#          'USER': 'Knight9z',
-#          'PASSWORD': 'Lzh20030114',
-#          'HOST': 'Knight9z.mysql.pythonanywhere-services.com',
+#          'NAME': 'projectdb',
+#          'USER':'root',
+#          'PASSWORD':'lzh20030114',
+#          'HOST':'127.0.0.1',
+#          'PORT':'3306',
 #      }
 # }
+DATABASES = {
+     'default': {
+         'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'Knight9z$projectdb',
+         'USER': 'Knight9z',
+         'PASSWORD': 'Lzh20030114',
+         'HOST': 'Knight9z.mysql.pythonanywhere-services.com',
+     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
